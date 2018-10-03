6f4dae15440c72b09728bcc9eb45d452018b80a2
etfrench <etfrench@email.wm.edu>
2018-10-02 21:47:02 -0400

    Updating unit tests for classes

--- test_classes.py
+++ test_classes.py
@@ -1,5 +1,5 @@
 """
-test.py
+classesTest.py
 Scope: Performs unit testing on the system to ensure proper functionality.
 Authors: Jeffrey Smith, Bill Cramer, Evan French
 """
@@ -7,7 +7,7 @@ import classes
 import unittest
 
 class ClassesTestClass(unittest.TestCase):
-	def SentenceStructureInitTest(self):
+	def test_SentenceStructureInit(self):
 		x = classes.SentenceStructure("I am a test string")
 		self.assertEqual(x.originalSentence, "I am a test string")
 		self.assertEqual(len(x.originalSentenceArray), 6)
@@ -16,7 +16,7 @@ class ClassesTestClass(unittest.TestCase):
 		self.assertEqual(x.originalSentenceArray[2][0], "a")
 		self.assertEqual(x.originalSentenceArray[3][0], "test")
 		self.assertEqual(x.originalSentenceArray[4][0], "string")
-		self.assertEqual(x.originalSentenceArray[4][0], "END")
+		self.assertEqual(x.originalSentenceArray[5][0], "END")
 		
 if __name__ == '__main__':
 	unittest.main()
